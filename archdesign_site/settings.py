b6c713518dcf8c6ed45efee3920d764692f8908f
Cicader <akromovich0928@gmail.com>
2022-06-19 17:45:52 +0500

    s

--- archdesign_site/settings.py
+++ archdesign_site/settings.py
@@ -133,11 +133,13 @@ MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = BASE_DIR / 'assets',
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
--- archdesign_site/urls.py
+++ archdesign_site/urls.py
@@ -15,16 +15,20 @@ Including another URLconf
 """
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ckeditror/',include('ckeditor_uploader.urls')),
+    path('ckeditror/', include('ckeditor_uploader.urls')),
+]
 
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+i18n_patterns(
+urlpatterns += i18n_patterns(
     path('i18n/', include('django.conf.urls.i18n')),
-    path('',include('blog.urls')),
+    path('', include('blog.urls'))
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
